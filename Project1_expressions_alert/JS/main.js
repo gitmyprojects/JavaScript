7740f370b7bc7301a0aea2c6663c278d086ae3b4
Neil Javier <portlandmmaguy@gmail.com>
2021-05-05 15:40:21 -0700

    initial

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+/*alert("Hello, World!");*/
+var A = "This is a string. "; //assign a value to variable A
+window.alert(A); //display the value of vaiable A
+var B = "\"ain\'t that a shame\" are lyics from a song," //using escape character
++ " but i can\'t remember who sang it."; //concatinating strings
+var C = A + B;
+document.write(C)
+var a = "Why was six afraid of seven?", b = "because...", c = "...seven ate nine."; //declare multiple variables at once
+var a = a.fontcolor("purple"); //practice assinging a font color to a string variable
+var b = b.fontcolor("red");
+var c = c.fontcolor("blue");
+document.write(a);
+var x = (3+3) //example of an expression
+document.write(x)
