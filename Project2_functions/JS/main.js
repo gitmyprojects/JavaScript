390d9c02872f05b2aeda5f88d71bb84fdaa90328
Neil Javier <portlandmmaguy@gmail.com>
2021-05-06 08:40:28 -0700

    push for instructor review

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+function conjunction(){
+    var a = "and" //declare variable
+    var b = "Because"
+    var c = (a) + " " + (b) //concatenate two variables with a space
+    document.getElementById("eye").innerHTML = document.write(c) //displays variable c
+}
+
+function forest(){
+     var action_one = "Run to the hills. "; //declare variable
+     action_one += "Run for your life. -Iron Maiden"; //concatenate variable with a string
+    document.getElementById("dee").innerHTML = action_one //displays result of concatenation on click of element with id "dee"
+
+}
